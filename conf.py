ad3b1b7d303f5ff891285ffb3f2792b3b593b768
Phil Lui <phillui37@gmail.com>
2025-08-31 13:26:20 +0800

    added isso comment support

--- conf.py
+++ conf.py
@@ -1034,12 +1034,12 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 #   disqus, discourse, facebook, intensedebate, isso, muut, commento,
 #   utterances
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = ""
+COMMENT_SYSTEM = "isso"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "http://localhost:8080"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
