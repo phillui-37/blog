1ceba16ed50ae42186c9511a6539780e6fa8527d
Phil Lui <phillui37@gmail.com>
2025-08-31 14:34:08 +0800

    fix comment system id

--- conf.py
+++ conf.py
@@ -1039,7 +1039,7 @@ COMMENT_SYSTEM = "isso"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "http://comments.kgy-production.xyz/"
+COMMENT_SYSTEM_ID = "https://kgy-production.xyz/isso/"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
